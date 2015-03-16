eae2761edfaf787b0f2904d9fd01393904baf3a1
zack Frazier <zackfrazier@the-air.local>
2015-03-16 19:08:31 -0400

    backup

---
+++ _zF/js/script.js
@@ -0,0 +1,16 @@
+/* Author: zack Frazier.
+
+*/
+
+var init = function init() {
+
+//$.getScript(Z.projectPath + 'js/jquery.animation.easing.js')
+//	$('#sg').animate({ fontSize:'100px' }, 1000);
+
+//	$('#scrim').delay(1000).fadeOut();
+
+
+
+}
+setTimeout(init,500);
+
