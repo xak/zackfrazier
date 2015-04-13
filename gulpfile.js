daba9bb1dcb38a961df5937fffbe731d08798b6c
zack Frazier <zack@zackfrazier.com>
2015-04-13 16:23:28 -0500

    adds css minifier

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ var imagemin = require('gulp-imagemin');
 var connect = require('connect');
 var serve = require('serve-static');
 var browsersync = require('browser-sync');
+var minifycss = require('gulp-minify-css');
+
+var reload      = browsersync.reload;
+
 
 // Copy Static Public Files
 gulp.task('public', function() {
@@ -20,6 +24,7 @@ gulp.task('html', function() {
     return gulp.src('./src/**/*.html')
         .pipe(gulp.dest('./dist/'));
 });
+gulp.task('html-watch', ['html'], browsersync.reload);
 
 // Process Images
 gulp.task('img', function() {
@@ -28,29 +33,33 @@ gulp.task('img', function() {
     .pipe(gulp.dest('./dist/img'));
 });
 
-
 // Process Styles
 gulp.task('css', function() {
-    return gulp.src('src/css/*.css')
+    return gulp.src('./src/css/*.css')
         .pipe(concat('zackfrazier.css'))
         .pipe(myth())
+        .pipe(minifycss())
         .pipe(gulp.dest('./dist/css/'));
 });
+gulp.task('css-watch', ['css'], browsersync.reload);
+
+
 
 // Process Scripts
 gulp.task('js', function() {
-    return gulp.src('src/js/*.js')
+    return gulp.src('./src/js/*.js')
     		.pipe(jshint())
     		.pipe(jshint.reporter('default'))
         .pipe(concat('zackfrazier.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js/'));
 });
+gulp.task('js-watch', ['js'], browsersync.reload);
 
 gulp.task('watch', function() {
-  gulp.watch('./src/*.html', gulp.series('html', browsersync.reload));
-  gulp.watch('./src/js/*.js', gulp.series('css', browsersync.reload));
-  gulp.watch('./src/css/*.css', gulp.series('css', browsersync.reload));
+  gulp.watch('./src/*.html', ['html-watch']);
+  gulp.watch('./src/js/*.js', ['js-watch']);
+  gulp.watch('./src/css/*.css', ['css-watch']);
 });
 
 
@@ -67,16 +76,23 @@ gulp.task('server', function() {
 gulp.task('browsersync', function(cb) {
     return browsersync({
         server: {
-            baseDir:'./src'
+            baseDir:'./dist'
         }
     }, cb);
 });
 
 
+// Build Task
+/*
+TO DO: separate dev and build processes
+
+gulp.task('build', ['public', 'img', 'html', 'css', 'js'], function () {
+ console.log('zackfrazier build is complete.')
+});
+*/
 
 // Default Task
 gulp.task('default', ['public', 'img', 'html', 'css', 'js', 'browsersync', 'watch'], function () {
- console.log('zackfrazier build is complete.')
  console.log('now watching html, css, and js.')
 });
 /*
