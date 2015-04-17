6281ac4f0d472eeff48c5c69c49e21d16b7ca920
zack Frazier <zfrazier@gmail.com>
2015-04-16 23:45:27 -0500

    updates song verse

--- src/js/components.js
+++ src/js/components.js
@@ -11,17 +11,54 @@ var SatisfactionGuarantee = React.createClass({
     );
   }
 });
+
 var Lyric = React.createClass({
   render: function() {
     return (
-			<div id="lyric">
-				"{ this.props.verse }" 
-				— 
-			  { this.props.artist }
-			</div>
+      <div id="lyric">
+        <div className="spotify" aria-hidden="true" role="presentation"/>
+        <p>
+          <span className="verse">
+            { this.props.data.verse }
+          </span>
+          <span className="artist">
+            { this.props.data.artist }, "{ this.props.data.song }"
+          </span>        
+        </p>
+      </div>
     );
   }
 });
+
+
+/*
+var Lyric = React.createClass({
+  render: function() {
+    return (
+			<a id="lyric" href={this.props.spotify}>
+        <span class="verse">
+          "{ this.props.verse }"
+        </span>
+        <br/> 
+        — 
+        <span class="artist">
+          { this.props.artist }
+        </span>        
+			</a>
+    );
+  }
+});
+
+var Link = React.createClass({
+  render: function() {
+    return (
+      <a href={ this.props.url }>
+
+      </a>
+    )
+  }
+});
+*/
 
 React.render(<SatisfactionGuarantee/>, document.getElementById('badge'));
 
@@ -30,11 +67,11 @@ console.log('Follow the dev of this site at https://github.com/xak/zackfrazier')
 Parse.Cloud.run('lyric', { }, {
   success: function(data) { 
     React.render(
-    	<Lyric verse={ data.verse } artist={ data.artist } song={ data.song } />, 
+    	<Lyric data={data} />, 
     	document.getElementById('banner')
     );
   },
   error: function(error) {
-    console.log('nope');
+    //console.log('nope');
   }
 });
