2857c79bb6e7ef5d9e67ae680e604447024ea461
zack Frazier <zfrazier@gmail.com>
2015-04-20 09:30:17 -0500

    adds animation panel

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 Parse.initialize("Ufa4Z1LS0KbzMNZlweb8jVWofxPO26tAmpqkRGt1", "9tTMikoTHpPgggsgo8NBfhJRN2KPMJZxh214fJcU");
 
 require('./components.js');
+require('./scrim.js');
 require('./main.js');
